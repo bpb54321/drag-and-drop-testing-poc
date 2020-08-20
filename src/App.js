b2c34c85d7b0b722ed4e537beeca0a8b3706ceaa
Brian Blosser <brian.blosser@wwt.com>
2020-08-19 20:53:02 -0400

    Get working react-testing-library test with react-beautiful-dnd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {DragDropContext, Draggable, Droppable} from 'react-beautiful-dnd';
+
 import './App.css';
 
+const singleColumnAreaId = 'single-column-layout-area';
+const sourceTrayId = 'source-tray';
+
+const items = [{
+  id: 1,
+  name: 'Item 1'
+}];
+
 function App() {
+  const [sourceTrayItems, setSourceTrayItems] = useState(items);
+  const [singleColumnAreaItems, setSingleColumnAreaItems] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DragDropContext
+        onDragStart={(result) => {
+          console.log('onDragStart')
+        }}
+        onDragUpdate={(result) => {
+          console.log('onDragUpdate')
+        }}
+        onDragEnd={(result) => {
+          const {destination, draggableId} = result;
+          if (destination.droppableId === singleColumnAreaId) {
+            const draggedItem = sourceTrayItems.find(
+              item => item.id.toString() === draggableId
+            );
+            setSourceTrayItems(items =>
+              items.filter(item => item.id.toString() !== draggableId)
+            );
+            setSingleColumnAreaItems(items => [...items, draggedItem]);
+          }
+          console.log('onDragEnd')
+        }}
+      >
+        <Droppable droppableId={sourceTrayId} direction="horizontal">
+          {droppableProvided => (
+            <div
+              className="library-item-card-container"
+              {...droppableProvided.droppableProps}
+              ref={droppableProvided.innerRef}
+              data-testid="source-tray"
+              style={{
+                backgroundColor: 'orange'
+              }}
+            >
+              {droppableProvided.placeholder}
+              {sourceTrayItems.map((item, index) => (
+                <Draggable
+                  draggableId={item.id.toString()}
+                  index={index}
+                  key={item.id}
+                >
+                  {draggableProvided => (
+                    <div
+                      {...draggableProvided.draggableProps}
+                      {...draggableProvided.dragHandleProps}
+                      ref={draggableProvided.innerRef}
+                      data-testid="library-item-card"
+                    >{item.name}</div>
+                  )}
+                </Draggable>
+              ))}
+            </div>
+          )}
+        </Droppable>
+        <Droppable droppableId={singleColumnAreaId} ignoreContainerClipping>
+          {provided => (
+            <div
+              className="layout-canvas"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              data-testid={singleColumnAreaId}
+              style={{
+                backgroundColor: 'yellow',
+                minHeight: '100px'
+              }}
+            >
+              {provided.placeholder}
+              {singleColumnAreaItems.map((item, index) => (
+                <Draggable
+                  draggableId={item.id.toString()}
+                  index={index}
+                  key={item.id}
+                >
+                  {draggableProvided => (
+                    <div
+                      {...draggableProvided.draggableProps}
+                      {...draggableProvided.dragHandleProps}
+                      ref={draggableProvided.innerRef}
+                      data-testid="library-item-card"
+                    >{item.name}</div>
+                  )}
+                </Draggable>
+              ))}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,149 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render, screen, fireEvent, act, waitFor} from '@testing-library/react';
 import App from './App';
+import {arrowDown, space} from "./KeyCodes";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('User can move an item from source tray to single column layout area', async () => {
+  jest.setTimeout(1000000);
+  jest.useFakeTimers();
+  Object.defineProperties(document.documentElement, {
+    clientHeight: {
+      value: 10000,
+      writable: true
+    },
+    clientWidth: {
+      value: 2650,
+      writable: true
+    }
+  });
+
+  render(<App/>);
+
+  const sourceTray = screen.getByTestId('source-tray');
+  Object.defineProperties(sourceTray, {
+    clientHeight: {
+      value: 120,
+      writable: true
+    },
+    clientWidth: {
+      value: 2650,
+      writable: true
+    },
+    getBoundingClientRect: {
+      value: jest.fn(() => {
+        console.log('sourceTray bounding client rect');
+        return {
+          "x": 0,
+          "y": 0,
+          "width": 2560,
+          "height": 20,
+          "top": 0,
+          "right": 2560,
+          "bottom": 20,
+          "left": 0
+        };
+      }),
+      writable: true
+    }
+  });
+  const singleColumnLayoutArea = screen.getByTestId(
+    'single-column-layout-area'
+  );
+  Object.defineProperties(singleColumnLayoutArea, {
+    clientHeight: {
+      value: 200,
+      writable: true
+    },
+    clientWidth: {
+      value: 2650,
+      writable: true
+    },
+    getBoundingClientRect: {
+      value: jest.fn(() => {
+        console.log('singleColumnLayoutArea bounding client rect');
+        return {
+          "x": 0,
+          "y": 20,
+          "width": 2560,
+          "height": 100,
+          "top": 20,
+          "right": 2560,
+          "bottom": 120,
+          "left": 0
+        };
+      }),
+      writable: true
+    }
+  });
+
+  const itemRectBeforeMove = {
+    "x": 0,
+    "y": 0,
+    "width": 2560,
+    "height": 20,
+    "top": 0,
+    "right": 2560,
+    "bottom": 20,
+    "left": 0
+  };
+  const itemRectAfterMove = {
+    "x": 0,
+    "y": 20,
+    "width": 2560,
+    "height": 20,
+    "top": 20,
+    "right": 2560,
+    "bottom": 40,
+    "left": 0
+  };
+  const itemGetBoundingClientRect = jest.fn();
+  itemGetBoundingClientRect.mockImplementationOnce(() => {
+    console.log('item bounding rect 1');
+    return itemRectBeforeMove;
+  });
+  itemGetBoundingClientRect.mockImplementationOnce(() => {
+    console.log('item bounding rect 2');
+    return itemRectBeforeMove;
+  });
+  itemGetBoundingClientRect.mockImplementationOnce(() => {
+    console.log('item bounding rect 3');
+    return itemRectAfterMove;
+  });
+
+  const item = screen.getByTestId('library-item-card');
+  Object.defineProperties(item, {
+    clientHeight: {
+      value: 20,
+      writable: true
+    },
+    clientWidth: {
+      value: 2650,
+      writable: true
+    },
+    getBoundingClientRect: {
+      value: itemGetBoundingClientRect,
+      writable: true
+    }
+  });
+  expect(sourceTray).toContainElement(item);
+  expect(singleColumnLayoutArea).not.toContain(item);
+
+  fireEvent.focus(item);
+  fireEvent.keyDown(item, {keyCode: space});
+  jest.runOnlyPendingTimers();
+
+  act(() => {
+    fireEvent.keyDown(item, {keyCode: arrowDown});
+    jest.runOnlyPendingTimers();
+  });
+
+  fireEvent.keyDown(item, {keyCode: space});
+  jest.runOnlyPendingTimers();
+
+
+  await waitFor(() => {
+    const itemAgain = screen.getByTestId('library-item-card');
+    expect(sourceTray).not.toContainElement(itemAgain);
+    expect(singleColumnLayoutArea).toContainElement(itemAgain);
+  });
 });
